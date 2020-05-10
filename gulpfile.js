57225709d4dcced6068fb4dc6d170d2a1b94b223
KirillGrant <grant323.43@gmail.com>
2020-05-11 00:58:12 +0300

    kakogo huya ne obnovlyaetsya avtomatom iz sass v css?????

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,22 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var concat = require('gulp-concat');
-var uglify=require('gulp-uglifyjs');
+var uglify = require('gulp-uglifyjs');
 
 gulp.task('Sass', function () {
-    return gulp.src('app/sass/main.sass')
+    return gulp.src('app/sass/**/*.sass')
         .pipe(sass())
         .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.stream())
 });
-
-gulp.task('Scripts',function f() {
+gulp.task('BrowserSync', function () {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        }
+    })
+});
+gulp.task('Scripts', function f() {
     return gulp.src([
         'app/libs/jquery/dist/jquery.js',
         'app/libs/magnific-popup/dist/jquery.magnific-popup.js'
@@ -19,16 +26,8 @@ gulp.task('Scripts',function f() {
         .pipe(uglify())
         .pipe(gulp.dest('app/js'));
 });
-
-
-gulp.task('BrowserSync', function () {
-    browserSync({
-        server: {
-            baseDir: 'app'
-        }
-    })
-});
-
-gulp.task('Watch', gulp.parallel('BrowserSync', 'Sass', 'Scripts') ,function () {
-    gulp.watch('app/sass/*.sass', gulp.parallel('Sass'))
+gulp.task('Watch', gulp.parallel('BrowserSync', 'Sass', 'Scripts'), function () {
+    gulp.watch('app/sass/*.sass',['Sass'])
+    gulp.watch('app/*.html', browserSync.reload)
+    gulp.watch('app/js/*.js', browserSync.reload)
 });
